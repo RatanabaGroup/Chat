be99340891ead01936c381a6248e40e2e7276e07
amandavo <amandavoliveira0@gmail.com>
2023-10-09 09:47:51 -0300

    header, modal cria grupos e botao para modal

--- project/src/pages/Dashboard/index.js
+++ project/src/pages/Dashboard/index.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity,
+        Image, Modal } from 'react-native';
 
 import auth from '@react-native-firebase/auth';
-import { useNavigation } from '@react-navigation/native';
-import SignIn from '../SignIn';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import AddButton from '../../components/AddButton';
+import ModalGrupo from '../../components/ModalGrupo';
 
 export default function Dashboard(){
   const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const [user, setUser] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(()=>{
+    const hasUser = auth().currentUser ? auth().currentUser.toJSON() : null;
+    // console.log(hasUser);
+    setUser(hasUser);
+
+  }, [isFocused]);
+
+  function handleSignOut(){
+    auth()
+    .signOut()
+    .then(()=>{
+      setUser(null);
+      navigation.navigate("SignIn")
+    })
+    .catch(()=>{
+      console.log("Nao ha usuario logado")
+    })
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>DASHBOARD = ChatRoom</Text>
-      <Button title="Login" 
-      onPress={ () => navigation.navigate(SignIn)}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        { user ? (
+          <TouchableOpacity onPress={handleSignOut}>
+            <MaterialIcons name="arrow-back" size={28} color="#FFF" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity/>
+        )}
+
+          <Image style={styles.logo} source={require('../SignIn/snow.png')} />
+
+        <TouchableOpacity>
+            <MaterialIcons name="search" size={28} color="#FFF" />
+          </TouchableOpacity>
+      </View>
+
+      <AddButton setVisible={ () => setModalVisible(true) }  userStatus={user} />
+
+      <Modal 
+        visible={modalVisible} 
+        animationType='fade'
+        transparent={true}
+      >
+        <ModalGrupo setVisible={ () => setModalVisible(false) } />
+      </Modal>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  container:{
+    flex:1,
+    backgroundColor: '#FFF'
   },
-});
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 34,
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    backgroundColor: '#2E54D4',
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  logo:{
+    width: 60,
+    height: 60
+  }
+})
